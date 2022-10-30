d9ad75faf458fa067b0f2542f999b266ae0045f4
First name <mailtosamim18@gmail.com>
2022-10-30 11:58:52 +0530

    Fixed bugs & added test client

--- testclient.py
+++ testclient.py
@@ -9,53 +9,53 @@ PASSWORD1 = 'helloworld'
 
 def test_register():
 
-	async def connect():
-		async with websockets.connect('ws://localhost:8765/register') as websocket:
-			await websocket.send(json.dumps({'username': USERNAME1, 'password': PASSWORD1}))
-			try:
-				while True:
-					response = json.loads(await websocket.recv())
-					print(response)
-					break
-			except:
-				pass
+    async def connect():
+        async with websockets.connect('ws://localhost:8765/register') as websocket:
+            await websocket.send(json.dumps({'username': USERNAME1, 'password': PASSWORD1}))
+            try:
+                while True:
+                    response = json.loads(await websocket.recv())
+                    print(response)
+                    break
+            except:
+                pass
 
-	asyncio.run(connect())
+    asyncio.run(connect())
 
 def test_login():
 
-	async def connect():
-		async with websockets.connect('ws://localhost:8765/login') as websocket:
-			await websocket.send(json.dumps({'username': USERNAME1, 'password': PASSWORD1}))
-			try:
-				while True:
-					response = json.loads(await websocket.recv())
-					break
-			except:
-				pass
-	asyncio.run(connect())
+    async def connect():
+        async with websockets.connect('ws://localhost:8765/login') as websocket:
+            await websocket.send(json.dumps({'username': USERNAME1, 'password': PASSWORD1}))
+            try:
+                while True:
+                    response = json.loads(await websocket.recv())
+                    break
+            except:
+                pass
+    asyncio.run(connect())
 
 def test_game_client(token):
 
-	async def connect():
-		async with websockets.connect('ws://localhost:8765/create-game'):
-			await websocket.send(json.dumps({'token': token['data']}))
-			try:
-				while True:
-					response = json.loads(await websocket.recv())
-			except:
-				pass
+    async def connect():
+        async with websockets.connect('ws://localhost:8765/create-game'):
+            await websocket.send(json.dumps({'token': token['data']}))
+            try:
+                while True:
+                    response = json.loads(await websocket.recv())
+            except:
+                pass
 
 def test_create_game(token):
 
-	async def connect():
-		async with websockets.connect('ws://localhost:8765/login') as websocket:
-			await websocket.send(json.dumps({'username': USERNAME1, 'password': PASSWORD1}))
-			try:
-				while True:
-					response = json.loads(await websocket.recv())
-					asyncio.run(test_game_client(token))
-					break
-			except:
-				pass
-	asyncio.run(connect())	
+    async def connect():
+        async with websockets.connect('ws://localhost:8765/login') as websocket:
+            await websocket.send(json.dumps({'username': USERNAME1, 'password': PASSWORD1}))
+            try:
+                while True:
+                    response = json.loads(await websocket.recv())
+                    asyncio.run(test_game_client(token))
+                    break
+            except:
+                pass
+    asyncio.run(connect())  
