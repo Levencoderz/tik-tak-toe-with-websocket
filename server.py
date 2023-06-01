ff091370ce4d8974c6832e0f5fc1177e6727ad77
Sk Samim <58543565+repostreet@users.noreply.github.com>
2023-06-01 16:38:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -177,7 +177,7 @@ class Server:
                             ERROR_MESSAGE = 'You have alreaady joined'
                             if value1['player_two'] and value1['player_two']:
                                 await websocket.send(json.dumps({'status': False, 'message': 'Please create a new game'}) 
-                            if value1['player_one'] == account['username']:
+                            elif value1['player_one'] == account['username']:
                                 await websocket.send(json.dumps({'status': False, 'message': ERROR_MESSAGE}))
                             elif value1['player_two'] == account['username']:
                                 await websocket.send(json.dumps({'status': False, 'message': ERROR_MESSAGE}))
